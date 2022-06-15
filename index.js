eb193ca9f323166b44f1d5a1771f0204c8c3747c
Florjan96 <florianshiqerukaj@yahoo.com>
2022-06-15 18:09:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let myItem1='Milk'
+let myItem1price=0.21
+
+let milk= myItem1 +" "+ "costs"+" "+ myItem1price +"$"
+
+console.log(milk)
+
+
+
+let myItem2='Cocoa'
+let myItem2price=2
+
+let cocoa= myItem2 +" "+ "costs"+" "+ myItem2price +"$"
+
+console.log(cocoa)
+
+
+
+let myItem3='Salad'
+let myItem3price=2
+
+let salad= myItem3 +" "+ "costs"+" "+ myItem3price +"$"
+
+console.log()
+
